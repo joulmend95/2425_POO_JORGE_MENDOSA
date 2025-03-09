3ca3ff3882ccf2d58e586be16ce1967276514b83
Jorge Mendosa <jl.mendosam@uea.edu.ec>
2025-03-09 10:52:12 -0500

    Semana 12
    Utilización de colecciones para la mejora de rendimiento en Sistema de Gestión de Biblioteca Digital

---
+++ Semana_12/Colecciones_en_Sistema_de_Biblioteca_Digital.py
@@ -0,0 +1,96 @@
+import json
+
+class Libro:
+    def __init__(self, isbn, titulo, autor ,categoria, prestado=False):
+        self.isbn = isbn
+        self.titulo = titulo
+        self.autor = autor
+        self.categoria = categoria
+        self.prestado = prestado
+
+    def to_dict(self):
+        return {
+            "isbn": self.isbn,
+            "titulo": self.titulo,
+            "autor": self.autor,
+            "categoria": self.categoria,
+            "prestado": self.prestado
+        }
+
+
+class Biblioteca:
+    def __init__(self, archivo_jason='biblioteca.jason'):
+        self.archivo_jason = archivo_jason
+        self.libros = self.cargar_libros()
+
+    def cargar_libros(self):
+        try:
+            with open(self.archivo_jason, 'r') as archivo:
+                datos_libros = json.load(archivo)
+                return {isbn: Libro(**datos) for isbn,datos in datos_libros.items()}
+        except FileNotFoundError:
+            return {}
+
+    def guardar_libros(self):
+        with open(self.archivo_jason, 'w') as archivo:
+            json.dump({isbn: libro.to_dict() for isbn,libro in self.libros.items()}, archivo, indent=4)
+
+
+    def añadir_libro(self, libro):
+        self.libros[libro.isbn] = libro
+        self.guardar_libros()
+
+
+    def prestar_libro(self, isbn):
+        libro = self.libros.get(isbn)
+        if libro and not libro.prestado:
+            libro.prestado = True
+            self.guardar_libros()
+            print(f"Libro {isbn} prestado con éxito.")
+        else:
+            print("Libro no disponible para préstar.")
+
+    def devolver_libro(self, isbn):
+        libro = self.libros.get(isbn)
+        if libro and libro.prestado:
+            libro.prestado = False
+            self.guardar_libros()
+            print(f"Libro {isbn} devuelto con éxito.")
+        else:
+            print("Error en la devolución del libro")
+
+    def mostrar_libros(self):
+        for libro in self.libros.values():
+            estado = "Prestado" if libro.prestado else "Disponible"
+            print(f"Libro {libro.isbn}: {libro.titulo} por {libro.autor} - {estado}")
+
+
+def menu():
+    biblioteca = Biblioteca()
+    while True:
+        print("\n1. Añadir Libro\n2. Mostrar Libros\n3. Prestar Libros\n4. Devolver Libro\n5. Salir")
+        opcion =input("Seleccione una opcion: ")
+        if opcion == "1":
+            isbn = input("Ingrese ISBN: ")
+            titulo = input("Ingrese titulo: ")
+            autor = input("Ingrese autor: ")
+            categoria = input("Ingrese categoría: ")
+            libro =Libro(isbn, titulo, autor, categoria)
+            biblioteca.añadir_libro(libro)
+        elif opcion == "2":
+            biblioteca.mostrar_libros()
+        elif opcion == "3":
+            isbn = input("ISBN del libro a prestar: ")
+            biblioteca.prestar_libro(isbn)
+        elif opcion == "4":
+            isbn = input("ISBN del libro a devolver: ")
+            biblioteca.devolver_libro(isbn)
+        elif opcion == "5":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción inválida. Intente nuevamente")
+
+
+if __name__ == "__main__":
+    menu()
